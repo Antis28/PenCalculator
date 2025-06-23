8e9dcc06fbefbe9fc96f44c3672b6f80c95e4ce5
Antis <antis28@mail.ru>
2025-06-23 20:26:45 +0300

    Форматирование вывода

---
+++ PenCalculator/Infrastructure/Services/StringFormat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenCalculator.Infrastructure.Services
+{
+    internal static class StringFormat
+    {
+        public static string FormatCulture(double val)
+        {
+            // Изменение культуры (локали) для замены разделителя на пробел
+            var cultureWithSpaceSeparator = new CultureInfo("ru-RU");
+            cultureWithSpaceSeparator.NumberFormat.NumberGroupSeparator = " ";
+            return Math.Round(val, 2).ToString("N", cultureWithSpaceSeparator);
+        }
+    }
+}
