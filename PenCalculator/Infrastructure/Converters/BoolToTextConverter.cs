12211a1ea0bd0b873044eb57defcdcd0a15a638c
Antis <antis28@mail.ru>
2026-09-10 21:01:38 +0300

    add конвертер для галочки скрыть/показать

---
+++ PenCalculator/Infrastructure/Converters/BoolToTextConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PenCalculator.Infrastructure.Converters
+{
+    public class BoolToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return b ? "Скрыть период" : "Показать период";
+
+            return "Показать детали";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
